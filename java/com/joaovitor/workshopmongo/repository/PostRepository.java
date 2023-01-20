4123638b0c364a52d66f6cb8a39b3fbfca977a6f
João Vitor <joaovitoralmeidas@hotmail.com>
2023-01-20 18:20:21 -0300

    Consulta simples com @Query

--- java/com/joaovitor/workshopmongo/repository/PostRepository.java
+++ java/com/joaovitor/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.joaovitor.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.joaovitor.workshopmongo.domain.Post;
@@ -10,7 +11,11 @@ import com.joaovitor.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
 }
 
 
